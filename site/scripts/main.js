60230802d5d923f59079e95e6a4d746e453e62c3
talrez11 <tal@way2cu.com>
2016-01-14 00:25:16 +0300

    Script: Added anchor animation function

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -51,6 +51,20 @@ Site.is_mobile = function() {
 Site.on_load = function() {
 	if (Site.is_mobile())
 		Site.mobile_menu = new Caracal.MobileMenu();
+
+	//Scroll Function
+	$('a[href*=#]').bind('click', function(e) {
+		e.preventDefault(); //prevent the "normal" behaviour which would be a "hard" jump
+
+		var target = $(this).attr("href"); //Get the target
+
+	// perform animated scrolling by getting top-position of target-element and set it as scroll target
+		$('html, body').stop().animate({ scrollTop: $(target).offset().top - 100 }, 1000, function() {
+		     location.hash = target;  //attach the hash (#jumptarget) to the pageurl
+		});
+
+		return false;
+	   });
 };
 
 
